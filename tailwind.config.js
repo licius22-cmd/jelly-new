a98ac401ead361401753a7a05cd4d823d0e36961
licius22-cmd <licius22@gmail.com>
2026-03-09 00:53:32 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,html}"
   ],
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
+      colors: {
+        primary: "#16a34a",
+        secondary: "#15803d",
+      }
     },
   },
   plugins: [],
