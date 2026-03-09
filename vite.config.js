ca05f949f37e053be959e109d95bf3a8a421b557
licius22-cmd <licius22@gmail.com>
2026-03-09 00:58:59 -0300

    Fix quotes and update build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-  plugins: [], // <-- É exatamente dessa linha vazia que a Cloudflare precisa
   build: {
-    outDir: 'dist',
+    outDir: "dist",
+    minify: "esbuild",
+    cssMinify: true,
+    sourcemap: false
   }
 });
